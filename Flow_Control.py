5ef0882b037903d61d69ba0939af317b49441c17
Poolerc <Poolerc@dukes.jmu.edu>
2017-04-25 21:48:10 -0400

    Add files via upload

---
+++ Flow_Control.py
@@ -0,0 +1,14 @@
+# Program to find the sum of all numbers stored in a list using for loop
+
+# List of numbers
+numbers = [6, 4, 3, 8, 4, 2, 5, 4, 11]
+
+# variable to store the sum
+sum = 0
+
+# list
+for val in numbers:
+	sum = sum+val
+
+# Output: The sum is 47
+print("The sum is", sum)
